931383bafd0fa69ae6bd952a4219974c61b439a0
孔昀晖 <1032316751@qq.com>
2017-08-04 16:09:40 +0800

    加入vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import FastClick from 'fastclick'
 import App from './App'
 import router from './router'
+import store from './vuex'
 import { ToastPlugin } from 'vux'
 
 Vue.use(ToastPlugin)
@@ -14,5 +15,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app-box')
